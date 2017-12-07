eff70e79728b60e941b07c62d7daadac83a85f35
Wico Wen <is@wico.cc>
2017-12-07 19:15:22 +0800

    ActivityIndicator demo

--- App.js
+++ App.js
@@ -1,47 +1,20 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, ListView, Text, View } from 'react-native';
+import { View } from 'react-native';
 
-export default class Movies extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true
-    }
-  }
+import { ActivityIndicator, AppRegistry, StyleSheet, Text } from 'react-native'
 
-  componentDidMount() {
-    return fetch('https://wico.cc/movies.json')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.setState({
-          isLoading: false,
-          dataSource: ds.cloneWithRows(responseJson.movies),
-        }, function() {
-          // do something with new state
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
 
+export default class App extends Component {
   render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={{flex: 1, paddingTop: 20}}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
-
     return (
-      <View style={{flex: 1, paddingTop: 20}}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text>{rowData.title}, {rowData.releaseYear}</Text>}
-        />
+      <View style={{flex: 1, flexDirection: 'row'}}>
+
+        <ActivityIndicator size="large" color="#0000ff" />
+        <ActivityIndicator size="small" color="#00ff00" />
+        <ActivityIndicator size="large" color="#0000ff" />
+        <ActivityIndicator size="small" color="#00ff00" />
+
       </View>
     );
   }
-}
+};
